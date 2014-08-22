1ee9fb4ba49c4c3cea4be5df5a5078a5c3cb25d9
Steven <steven2358@gmail.com>
2014-08-22 11:45:35 +0200

    Step 3: Extracting features

--- src/feedForwardAutoencoder.m
+++ src/feedForwardAutoencoder.m
@@ -14,6 +14,7 @@ b1 = theta(2*hiddenSize*visibleSize+1:2*hiddenSize*visibleSize+hiddenSize);
 %% ---------- YOUR CODE HERE --------------------------------------
 %  Instructions: Compute the activation of the hidden layer for the Sparse Autoencoder.
 
+activation = sigmoid(W1*data + repmat(b1,1,size(data,2)));
 
 %-------------------------------------------------------------------
 
